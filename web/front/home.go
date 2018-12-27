38fae1fa729ec0bc0dbb4a4763b91435dd1edf30
Edouard Paris <m@edouard.paris>
2018-12-27 17:36:24 +0100

    fix view home

--- web/front/home.go
+++ web/front/home.go
@@ -5,17 +5,17 @@ import (
 
 	"git.iiens.net/edouardparis/town/app"
 	"git.iiens.net/edouardparis/town/failures"
+	"git.iiens.net/edouardparis/town/resources"
 	"git.iiens.net/edouardparis/town/web/middlewares"
 )
 
 func Home(a *app.App, handle middlewares.HandleError) http.HandlerFunc {
+	data := struct {
+		Header *resources.Header
+	}{}
 	return func(w http.ResponseWriter, r *http.Request) {
-		home := struct {
-			Title string
-		}{
-			Title: "HEllo",
-		}
-		err := render(w, r, "home.html", home, http.StatusOK)
+		data.Header = resources.NewHeader(a.Info)
+		err := render(w, r, "home.html", data, http.StatusOK)
 		if err != nil {
 			handle(w, r, failures.ErrNotFound)
 		}
