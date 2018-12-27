32d50bdb7d091b844399bbd7bfb0f952dc4377af
Edouard Paris <m@edouard.paris>
2018-12-27 09:44:29 +0100

    ft article detail

--- web/front/articles.go
+++ web/front/articles.go
@@ -1,13 +1,13 @@
 package front
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
 
 	"git.iiens.net/edouardparis/town/app"
 	"git.iiens.net/edouardparis/town/failures"
+	"git.iiens.net/edouardparis/town/resources"
 	"git.iiens.net/edouardparis/town/web/middlewares"
 )
 
@@ -22,13 +22,20 @@ func articlesRoutes(a *app.App) func(r chi.Router) {
 }
 
 func ArticleDetail(a *app.App, handle middlewares.HandleError) http.HandlerFunc {
+	data := struct {
+		Article *resources.Article
+	}{}
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		article, ok := middlewares.ArticleFromCtx(ctx)
-		if !ok {
+		if !ok || article == nil {
 			handle(w, r, failures.ErrNotFound)
 			return
 		}
-		w.Write([]byte(fmt.Sprintf("title:%s, address:%s", article.Title, article.Address.Value)))
+		data.Article = resources.NewArticle(article)
+		err := render(w, r, "article.html", data, http.StatusOK)
+		if err != nil {
+			handle(w, r, failures.ErrNotFound)
+		}
 	}
 }
