41ec97343aa300e5c9a2e7e9737b6ecefdb2f2c8
Edouard Paris <m@edouard.paris>
2019-01-06 16:55:50 +0100

    revert main poc

--- main.go
+++ main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
+	"context"
+	"flag"
 
-	_ "github.com/lib/pq"
+	"github.com/EdouardParis/town/app"
+	"github.com/EdouardParis/town/web/server"
 )
 
-func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		os.Getenv("POSTGRESQL_ADDON_HOST"),
-		os.Getenv("POSTGRESQL_ADDON_PORT"),
-		os.Getenv("POSTGRESQL_ADDON_USER"),
-		os.Getenv("POSTGRESQL_ADDON_PASSWORD"),
-		os.Getenv("POSTGRESQL_ADDON_DB"))
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-}
-
-/*
 func main() {
 	var configFile string
 	flag.StringVar(&configFile, "c", "", "config file")
@@ -49,4 +26,3 @@ func main() {
 	err = server.Run(context.Background(), app)
 	app.Logger.Info(err.Error())
 }
-*/
