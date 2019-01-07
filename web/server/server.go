412f7108e26e35ffa8cb2ce79051842659bb92d1
Edouard Paris <m@edouard.paris>
2019-01-07 22:49:17 +0100

    fix server port listening

--- web/server/server.go
+++ web/server/server.go
@@ -12,6 +12,7 @@ import (
 	"golang.org/x/sync/errgroup"
 
 	"github.com/EdouardParis/town/app"
+	"github.com/EdouardParis/town/logging"
 )
 
 // Run the server
@@ -34,12 +35,16 @@ func Run(ctx context.Context, app *app.App) error {
 	// HTTP server
 	g.Go(func() error {
 		cerr := make(chan error)
+		addr := os.Getenv("PORT")
+		if addr == "" {
+			addr = ":8080"
+		}
 		srv := http.Server{
-			Addr:    ":8080",
+			Addr:    addr,
 			Handler: Routes(ctx, app),
 		}
 
-		app.Logger.Info("listening on port :8080")
+		app.Logger.Info("server listening", logging.String("port", addr))
 		go func() { cerr <- errors.WithStack(srv.ListenAndServe()) }()
 		select {
 		case err := <-cerr:
